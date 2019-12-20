8a0df188878698445dc07634244139616942d494
Sander F. Sandøy <sanderfsandoy@hotmail.com>
2019-12-20 14:05:47 +0100

    - Added toggle of more or less filters (WIP)

--- src/components/search/wineform/WineSearchForm.js
+++ src/components/search/wineform/WineSearchForm.js
@@ -15,6 +15,7 @@ import ImageCheckbox from "components/add-wine/image-checkbox/image-checkbox";
 // TODO TYPESCRIPT.
 const WineSearchFormComponent = props => {
   const [wineName, setWineName] = useState("");
+  const [expandedFilter, setExpandedFilter] = useState(false);
   // TODO FIX DEFAULT VALUES SO THAT PLACEHOLDER IS SHOWN.
   const [selectedWineGrapes, setSelectedWineGrapes] = useState([]);
   const [selectedCountries, setSelectedCountries] = useState([]);
@@ -82,104 +83,122 @@ const WineSearchFormComponent = props => {
               onChange={e => setWineName(e.target.value)}
             />
           </div>
-          <div className="col-12">
-            <label>Drue</label>
-            <SearchDropDown
-              placeholder="Vindrue"
-              searchItems={wineGrapeItems}
-              selectedItems={selectedWineGrapes.map(grape => ({
-                label: grape,
-                value: grape
-              }))}
-              onClick={grapeArray => setSelectedWineGrapes(grapeArray)}
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="fitsTo">Land</label>
-            <SearchDropDown
-              placeholder="Land"
-              selectedItems={selectedCountries.map(country => ({
-                label: country,
-                value: country
-              }))}
-              searchItems={[
-                ...new Set(props.allWines.map(wine => wine.wineCountry))
-              ]}
-              onClick={countryArray => setSelectedCountries(countryArray)}
-            />
-          </div>
-          <div className="col-12">
-            <label>Region</label>
-            <SearchDropDown
-              placeholder="Region"
-              selectedItems={selectedRegions.map(region => ({
-                label: region,
-                value: region
-              }))}
-              searchItems={[
-                ...new Set(
-                  props.allWines
-                    .map(wine => wine.wineRegion)
-                    .filter(region => region !== null)
-                )
-              ]}
-              onClick={regionArray => setSelectedRegions(regionArray)}
-            />
-          </div>
-          <div className="col-6">
-            <label>Rating Sander</label>
-            <div className="col-12 wine-search-form__slider-rating">
-              <Slider
-                className="react-slider"
-                defaultValue={sanderRating}
-                getAriaValueText={value => value}
-                aria-labelledby="discrete-slider-always"
-                valueLabelDisplay="auto"
-                onChange={(event, value) => setSanderRating(value)}
-                step={0.1}
-                marks={marks}
-                min={0}
-                max={10}
-              />
-            </div>
-          </div>
-          <div className="col-6">
-            <label>Rating Ine</label>
-            <div className="col-12 wine-search-form__slider-rating">
-              <Slider
-                className="react-slider"
-                defaultValue={ineRating}
-                getAriaValueText={value => value}
-                aria-labelledby="range-slider"
-                onChange={(event, value) => setIneRating(value)}
-                valueLabelDisplay="auto"
-                step={0.1}
-                marks={marks}
-                min={0}
-                max={10}
-              />
-            </div>
-          </div>
         </div>
-        <div className="col-12">
-          <label htmlFor="fitsTo">Passer til</label>
-          <div className="row fits-to-row">
-            {imageKeys.map(imageKey => (
-              <ImageCheckbox
-                key={imageKey + "searchForm"}
-                columnProps="col-4 fits-to-cell"
-                image={images[imageKey]}
-                htmlFor={imageKey + "searchForm"}
-                value={imageKey}
-                name="fitsToSearchForm"
-                onChange={event =>
-                  setSelectedFitsTo(
-                    pushOrRemoveToArray(selectedFitsTo, event.target.value)
-                  )
-                }
+        {expandedFilter && (
+          <div className="row">
+            <div className="col-12">
+              <label>Drue</label>
+              <SearchDropDown
+                placeholder="Vindrue"
+                searchItems={wineGrapeItems}
+                selectedItems={selectedWineGrapes.map(grape => ({
+                  label: grape,
+                  value: grape
+                }))}
+                onClick={grapeArray => setSelectedWineGrapes(grapeArray)}
               />
-            ))}
+            </div>
+            <div className="col-12">
+              <label htmlFor="fitsTo">Land</label>
+              <SearchDropDown
+                placeholder="Land"
+                selectedItems={selectedCountries.map(country => ({
+                  label: country,
+                  value: country
+                }))}
+                searchItems={[
+                  ...new Set(props.allWines.map(wine => wine.wineCountry))
+                ]}
+                onClick={countryArray => setSelectedCountries(countryArray)}
+              />
+            </div>
+            <div className="col-12">
+              <label>Region</label>
+              <SearchDropDown
+                placeholder="Region"
+                selectedItems={selectedRegions.map(region => ({
+                  label: region,
+                  value: region
+                }))}
+                searchItems={[
+                  ...new Set(
+                    props.allWines
+                      .map(wine => wine.wineRegion)
+                      .filter(region => region !== null)
+                  )
+                ]}
+                onClick={regionArray => setSelectedRegions(regionArray)}
+              />
+            </div>
+            <div className="col-6">
+              <label>Rating Sander</label>
+              <div className="col-12 wine-search-form__slider-rating">
+                <Slider
+                  className="react-slider"
+                  defaultValue={sanderRating}
+                  getAriaValueText={value => value}
+                  aria-labelledby="discrete-slider-always"
+                  valueLabelDisplay="auto"
+                  onChange={(event, value) => setSanderRating(value)}
+                  step={0.1}
+                  marks={marks}
+                  min={0}
+                  max={10}
+                />
+              </div>
+            </div>
+            <div className="col-6">
+              <label>Rating Ine</label>
+              <div className="col-12 wine-search-form__slider-rating">
+                <Slider
+                  className="react-slider"
+                  defaultValue={ineRating}
+                  getAriaValueText={value => value}
+                  aria-labelledby="range-slider"
+                  onChange={(event, value) => setIneRating(value)}
+                  valueLabelDisplay="auto"
+                  step={0.1}
+                  marks={marks}
+                  min={0}
+                  max={10}
+                />
+              </div>
+            </div>
           </div>
+        )}
+        {expandedFilter && (
+          <div className="col-12">
+            <label htmlFor="fitsTo">Passer til</label>
+            <div className="row fits-to-row">
+              {imageKeys.map(imageKey => (
+                <ImageCheckbox
+                  key={imageKey + "searchForm"}
+                  columnProps="col-4 fits-to-cell"
+                  image={images[imageKey]}
+                  htmlFor={imageKey + "searchForm"}
+                  value={imageKey}
+                  name="fitsToSearchForm"
+                  onChange={event =>
+                    setSelectedFitsTo(
+                      pushOrRemoveToArray(selectedFitsTo, event.target.value)
+                    )
+                  }
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="wine-search-form__toggle-filter">
+          <button
+            type="submit"
+            className="wine-search-form__button btn btn-primary"
+            onClick={() => {
+              setExpandedFilter(!expandedFilter);
+              window.scrollTo(0, 0);
+            }}
+          >
+            {expandedFilter ? "Skjul ekstra filter" : "Vis flere filter"}
+          </button>
         </div>
         <div className="row wine-search-form__buttons">
           <div className="col-6">
