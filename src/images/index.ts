8d09e78fdd8e71a635e7fcb5ad0740da31f997f2
Sander F. Sandøy <sanderfsandoy@hotmail.com>
2021-06-11 14:24:19 +0200

    - Addd hamburger-menu for native

--- src/images/index.ts
+++ src/images/index.ts
@@ -3,6 +3,8 @@ export { default as seafood } from "./crab.png";
 export { default as apetirif } from "./apetirif.png";
 export { default as cheese } from "./cheese.png";
 export { default as chicken } from "./chicken.png";
+export { default as cake } from "./cake.png";
+export { default as fish } from "./fish.png";
 export { default as pasta } from "./pasta.png";
 export { default as pizza } from "./pizza.png";
 export { default as pig } from "./pig.png";
@@ -10,7 +12,9 @@ export { default as bull } from "./bull.png";
 
 export const imageKeys = [
   "chicken",
+  "cake",
   "seafood",
+  "fish",
   "pasta",
   "pizza",
   "cheese",
